a3c7a0de110dcbca8de1ae8fc01b4879ff859e93
NerdiumLol <34074963+NerdiumLol@users.noreply.github.com>
2019-10-09 12:10:56 -0500

    Add map functions

--- src/engine/map/Map.java
+++ src/engine/map/Map.java
@@ -4,4 +4,24 @@ import config.MapConfig;
 
 public class Map {
 	private String[][] mapData = new String[MapConfig.width][MapConfig.height];
+	
+	public void generateMap() {
+		for(int i = 0; i < MapConfig.height; i++) {
+			for(int j = 0; j < MapConfig.width; j++) {
+				mapData[j][i] = "";
+			}
+		}
+	}
+	
+	public String idAtPos(int x, int y) {
+		return mapData[y][x];
+	}
+	
+	public void placeEntity(String id, int x, int y, int sizeX, int sizeY) {
+		for(int i = 0; i < sizeY; i++) {
+			for(int j = 0; j < sizeX; j++) {
+				mapData[y+i][x+j] = id;
+			}
+		}
+	}
 }
